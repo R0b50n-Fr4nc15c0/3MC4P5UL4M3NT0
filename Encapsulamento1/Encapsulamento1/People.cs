a28b2d09df308593028703f655f79affec90ea12
R0b50n-Fr4nc15c0 <robson.ninecraft@gmail.com>
2024-11-08 14:43:45 -0300

    Add files via upload

---
+++ Encapsulamento1/Encapsulamento1/People.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento1
+{
+    public class People
+    {
+        private string nome;
+        private int idade;
+        private string tele;
+        private string endereço;
+        private string email;
+
+        public string Nome
+        {
+            get
+            { return (string)nome; }
+            set
+            { if (value != "") nome = value; }
+
+        }
+        public int Idade { 
+            get 
+            { return (int)(idade * 0.7); }
+            set 
+            { if (value >= 0) idade = value; }
+        }
+
+        public string Tele
+        {
+            get
+            { return (string)tele; }
+            set
+            { if (value != "") tele = value; }
+
+        }
+        public string Endereço
+        {
+            get
+            { return (string)endereço;  }
+            set
+            { if (value != "") endereço = value; }
+        }
+        public string Email
+        {
+            get
+            { return (string)email; }
+            set
+            { if (value != "") email = value; }
+        }
+    }
+}
---
+++ Encapsulamento1/Encapsulamento1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encapsulamento1;
+
+People p = new People();
+
+
+p.Nome = "Robson";
+p.Idade = 22;
+p.Tele = "(81) 98932-4351";
+p.Endereço = "54517-110";
+p.Email = "robson.@aluno.senai.br";
+
+
+Console.WriteLine($"O nome dessa pessoa é: {p.Nome}");
+Console.WriteLine($"Essa pessoa tem {p.Idade} anos");
+Console.WriteLine($"O telefone dessa pessoa é: {p.Tele}");
+Console.WriteLine($"O endereço dessa pessoa é: {p.Endereço}");
+Console.WriteLine($"O Email dessa pessoa é: {p.Email}");
+
+Console.ReadKey();
